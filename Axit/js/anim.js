06b3a24d9db881ed061b691cd20b62c4d50c74c3
oldosxaksli <johndoe@example.com>
2019-11-19 21:57:15 +0500

    edit js

--- Axit/js/anim.js
+++ Axit/js/anim.js
@@ -47,7 +47,7 @@ footer.style.transform = "translate(100%, 0)";
 setInterval(() => {
 	console.log(scrollY);
 
-	if (scrollY + 330 >= mainWrap.offsetTop) {
+	if (scrollY + 400 >= mainWrap.offsetTop) {
 			setTimeout(() => {
 			mainWrap.style.opacity = "1";
 			mainWrap.style.transition = "all .2s ease";
@@ -55,55 +55,55 @@ setInterval(() => {
 		}, 1)
 	};
 
-	if (scrollY + 330 >= socialMedia.offsetTop) {
+	if (scrollY + 400 >= socialMedia.offsetTop) {
 		socialMedia.style.opacity = "1";
 		socialMedia.style.transition = "all .2s ease";
 		socialMedia.style.transform = "translate(0, 0)";
 	};
 
-	if (scrollY + 330 >= tabs.offsetTop) {
+	if (scrollY + 400 >= tabs.offsetTop) {
 		tabs.style.opacity = "1";
 		tabs.style.transition = "all .2s ease";
 		tabs.style.transform = "translate(0, 0)";
 	};
 
-	if (scrollY + 330 >= sub.offsetTop) {
+	if (scrollY + 400 >= sub.offsetTop) {
 		sub.style.opacity = "1";
 		sub.style.transition = "all .2s ease";
 		sub.style.transform = "translate(0, 0)";
 	};
 
-	if (scrollY + 330 >= pictureSection.offsetTop) {
+	if (scrollY + 400 >= pictureSection.offsetTop) {
 		pictureSection.style.opacity = "1";
 		pictureSection.style.transition = "all .2s ease";
 		pictureSection.style.transform = "translate(0, 0)";
 	};
 
-	if (scrollY + 330 >= whyThis.offsetTop) {
+	if (scrollY + 400 >= whyThis.offsetTop) {
 		whyThis.style.opacity = "1";
 		whyThis.style.transition = "all .2s ease";
 		whyThis.style.transform = "translate(0, 0)";
 	};
 
-	if (scrollY + 330 >= pricingOptions.offsetTop) {
+	if (scrollY + 400 >= pricingOptions.offsetTop) {
 		pricingOptions.style.opacity = "1";
 		pricingOptions.style.transition = "all .2s ease";
 		pricingOptions.style.transform = "translate(0, 0)";
 	};
 
-	if (scrollY + 330 >= what.offsetTop) {
+	if (scrollY + 400 >= what.offsetTop) {
 		what.style.opacity = "1";
 		what.style.transition = "all .2s ease";
 		what.style.transform = "translate(0, 0)";
 	};
 
-	if (scrollY + 330 >= stylish.offsetTop) {
+	if (scrollY + 400 >= stylish.offsetTop) {
 		stylish.style.opacity = "1";
 		stylish.style.transition = "all .2s ease";
 		stylish.style.transform = "translate(0, 0)";
 	};
 
-	if (scrollY + 330 >= contact.offsetTop) {
+	if (scrollY + 400 >= contact.offsetTop) {
 		contact.style.opacity = "1";
 		contact.style.transition = "all .2s ease";
 		contact.style.transform = "translate(0, 0)";
