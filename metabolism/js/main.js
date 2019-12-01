163a666ab1557d6d4859a043b7871c17c8d59add
oldosxaksli <johndoe@example.com>
2019-12-01 13:57:35 +0500

    add metabolism

---
+++ metabolism/js/main.js
@@ -0,0 +1,5 @@
+let btn = document.querySelector(".btn-form");
+let select = document.querySelector(".select");
+let massa = document.querySelector(".massa");
+let age = document.querySelector("age");
+
