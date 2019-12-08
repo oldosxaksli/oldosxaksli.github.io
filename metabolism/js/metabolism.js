a1c40938310003b9ceafc5d5f1fc7278d18904b0
oldosxaksli <johndoe@example.com>
2019-12-08 12:51:05 +0500

    upgrade

---
+++ metabolism/js/metabolism.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector(".btn-form");
+let select = document.querySelector(".select");
+let massa = document.querySelector(".massa");
+let age = document.querySelector(".age");
+let res = 0;
+
+function run () {
+	if (mass.value >= 20 && massa.value <= 120) {
+		console.log("valid massa true");
+	}
+	else if (age.value >= 10 && age.value <= 100) {
+		console.log("valid age true");
+	}
+
+
+
+
+
+
+
+
+}
